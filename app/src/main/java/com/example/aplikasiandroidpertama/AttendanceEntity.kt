003f34ec366f1542416f445478d6f87051509def
Alda <aldarahmayanti04@gmail.com>
2025-12-18 15:45:50 +0700

    commit

---
+++ app/src/main/java/com/example/aplikasiandroidpertama/AttendanceEntity.kt
@@ -0,0 +1,21 @@
+package com.example.aplikasiandroidpertama
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+@Entity(
+    tableName = "attendance",
+    foreignKeys = [
+        ForeignKey(
+            entity = UserEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["user_id"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+class AttendanceEntity {
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
+}
